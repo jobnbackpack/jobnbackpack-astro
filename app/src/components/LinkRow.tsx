8e7fe1ce9bdddc08b8206328aa005900c9e3d885
Robin Janzen <rjjanzen@web.de>
2022-09-29 19:09:26 +0200

    feat: added styled-components

---
+++ app/src/components/LinkRow.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styled from "styled-components";
+
+function LinkRow() {
+  const Row = styled.div`
+    color: red;
+    font-size: 200px;
+  `;
+
+  return <Row>saonethu</Row>;
+}
+
+export default LinkRow;
