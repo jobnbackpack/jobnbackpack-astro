1038bcf4fa65e6ca7c9dbbad5b7e61047fbc7905
Robin Janzen <rjanzen@conet.de>
2023-04-20 11:10:46 +0200

    chore: rm analytics package + add vercel package

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import vercel from '@astrojs/vercel/static'
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), tailwind()],
+    adapter: vercel({analytics: true})
 });
